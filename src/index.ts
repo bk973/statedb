d221494ac69655e489c5c35b7ac84f508f13d932
Benjamin Kalungi <benkalungi256@gmail.com>
2021-09-15 15:02:10 +0000

    FEAT: Add collection methods instance methods

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,47 @@ class CreateDatabase {
  * @param {Object} shape - The shape that will be taken by the data in the collection
  */
 class Collection {
-  constructor() {
+  name: string;
+  shape: any; 
+
+  constructor(name: string, shape:any) {
+       this.name = name; //Required name of the collection...
+       this.shape = shape; //The shape that will be adopted by the incoming data...
+  }
+
+  /**
+   * Inserts new data into the collection
+   * @param data {Object} An object containing data to be saved to the collection
+   */
+  save(/**data */): void{
+      //evaluates new Field(id, data) NOTE: id must be unique for every new record
+      
+      //generate a random id here
+      
+  }
+  /**
+   * Queries the collection for data based on the query object.
+   * @param query {Object} an object describing the type of query that will be used to retrieve data.
+   */
+  find(/**Query {} */): any{
 
   }
+
+  /**
+   * Return a single record from a collection based on the parameters
+   */
+  findOne(/**params */){
+
+  }
+
+  /**
+   * Return a single record from the collection based on its unique ID.
+   * @param id {string} the ID of the record to be found.
+   */
+  findById(/**id */){
+
+  }
+
 }
 
 /**
@@ -24,7 +62,11 @@ class Collection {
  * @param {Object} record - A new object holding the data to be inserted into a collection.
  */
 class Field {
-    constructor(){
+    id: string;
+    data: any;
 
+    constructor(id:string, data:any){
+           this.id = id;
+           this.data = data;
     }
 }
