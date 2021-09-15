2cc857aea054cf94a52b0a354684158c3a25746d
Benjamin Kalungi <benkalungi256@gmail.com>
2021-09-15 13:57:37 +0000

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,30 @@
-class Collection {
-  constructor(name: string) {
-    this.name = name;
-    this.docs = [];
+/**
+ * Creates a new database instance
+ * @param {string} name - The optional database name.
+ */
+class CreateDatabase {
+  constructor() {
+
   }
-
-  /**
-   * The save method is used to retrieve records from the database.
-   * @param {object} -
-   *
-   */
-  find() {}
-
-  /**
-   * method used to sort the order of records returned from the find() method
-   * there are two sort orders namely; positive, negative
-   * @param {number} - 1 represents ascending, -1 represents descending order
-   */
-  sort() {}
-
-  /**
-   * The save method is used to save new records (documents) to the database.
-   * @param {object}
-   *
-   */
-  save() {}
-
-  /**
-   * Method used to update a particular document in a collection
-   * @param {string}
-   *
-   */
-  delete() {}
-
-  /**
-   * Method used to update a particular document in a collection
-   * @param {string}
-   *
-   */
-  update() {}
 }
 
-class CreateDatabase {
-  constructor(name: string) {
-    this.name = name; //The name of the database instance
-    this.collections = []; // This is where all the collections will live
-  }
+/**
+ * Creates a new collection. This class is only used by CreateDatabase class
+ * @param {string} name - The name of the collection is a require parameter.
+ * @param {Object} shape - The shape that will be taken by the data in the collection
+ */
+class Collection {
+  constructor() {
 
-  /***
-   * Method used to create a new collection and add it to the database
-   * @param {string} - The name of the collection to be created.
-   */
-  createCollection(name: string) {
-    const _collection = new Collection(name);
-    //add the new created collection to the database's
-    // list of collections...
-    this.collections.push(_collection)
   }
+}
 
-  /**
-   * Used to delete a specific collection from the
-   * database...
-   * @param {string} - The name of the collection to be deleted...
-   */
-  deleteCollection(name: string) {}
+/**
+ * Create a new record. This class is only used by Collection class.
+ * @param {Object} record - A new object holding the data to be inserted into a collection.
+ */
+class Field {
+    constructor(){
+
+    }
 }
