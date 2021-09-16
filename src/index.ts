daeb3eb113c207356ea125758ade107225e94c0f
Benjamin Kalungi <benkalungi256@gmail.com>
2021-09-16 10:06:56 +0000

    CHORE(master): Adds basic class members and their type annotations

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
 /**
  * Creates a new database instance
- * @param {string} name - The optional database name.
+ * @param name {string} - The optional database name.
  */
 class CreateDatabase {
-  constructor() {
-
+      name?: string; //optional
+      /**
+       * NOTE: We hope to add another class member to accept an options Object for config purposes.
+       */
+  constructor(name?: string) {
+      this.name = name;
   }
 }
 
 /**
  * Creates a new collection. This class is only used by CreateDatabase class
- * @param {string} name - The name of the collection is a require parameter.
- * @param {Object} shape - The shape that will be taken by the data in the collection
+ * @param  name  {string} - The name of the collection is a require parameter.
+ * @param  shape {Object}  - The shape that will be taken by the data in the collection
  */
 class Collection {
-  constructor() {
-
+    name: string; //required
+    shape: object; //required
+  constructor(name: string, shape: object) {
+    this.name = name;
+    this.shape = shape;
   }
 }
 
 /**
  * Create a new record. This class is only used by Collection class.
- * @param {Object} record - A new object holding the data to be inserted into a collection.
+ * @param record {Object}  - A new object holding the data to be inserted into a collection.
  */
 class Field {
-    constructor(){
+    id: string; //required
+    data: object; //required
 
+    constructor(id: string, data: object){
+        this.id = id;
+        this.data = data;
     }
 }
