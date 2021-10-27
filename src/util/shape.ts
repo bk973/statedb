c813a16e07dcc6547031b3f918f037bc82a80bdf
Benjamin Kalungi <benkalungi256@gmail.com>
2021-10-27 09:52:50 +0000

    update(master): Adds shape util function for defining collection data shape

---
+++ src/util/shape.ts
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param shape {Object} - Object containing the shape of collection data.
+ * @returns shape {Object} - Object representing the shape of collection data.
+ */
+const shape = function(shape: object): object{
+      return shape;
+}
