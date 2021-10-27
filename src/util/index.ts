f30cfc97af0b84ff150bc7465208a7c39fbd545b
Benjamin Kalungi <benkalungi256@gmail.com>
2021-10-27 10:27:13 +0000

    update(master): adds all utils to index

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,6 +3,13 @@
  * @param shape {Object} - Object containing the shape of collection data.
  * @returns shape {Object} - Object representing the shape of collection data.
  */
-const shape = function(shape: object): object{
+export const shape = function(shape: object): object{
       return shape;
+}
+
+export const types = {
+       string:  'string',
+       number: 'number',
+       boolean: 'boolean',
+       array: 'array',
 }
